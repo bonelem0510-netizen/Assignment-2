a2b850902a1e64c350d8ab8e3435ae5a15dcff10
bonelem0510-netizen <bonelem0510@gmail.com>
2026-04-22 12:13:19 +0200

    added buttons and textViews functions to MainActivity.kt

--- app/src/main/java/com/example/assignment220/MainActivity.kt
+++ app/src/main/java/com/example/assignment220/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.assignment220
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -35,4 +37,76 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.act_questions)
     }
+
+
+    private fun showWelcomeScreen() {
+        setContentView(R.layout.act_welcome)
+        val startButton = findViewById<Button>(R.id.btnShow)
+        startButton.setOnClickListener {
+            currentQuestions = 0
+            score = 0
+            feedbackList.clear()
+            showQuestionScreen()
+        }
+    }
+
+    private fun showQuestionScreen() {
+        setContentView(R.layout.act_questions)
+
+        val questionText = findViewById<TextView>(R.id.questionText)
+        val trueButton = findViewById<Button>(R.id.trueButton)
+        val falseButton = findViewById<Button>(R.id.falseButton)
+        val feedbackText = findViewById<TextView>(R.id.feedbackText)
+        val nextButton = findViewById<Button>(R.id.nextButton)
+
+        questionText.text = questions[currentQuestions]
+        feedbackText.text = ""
+
+        var answered = false
+
+        trueButton.setOnClickListener {
+            if (!answered) {
+                checkAnswer(true, feedbackText)
+                answered = true
+            }
+        }
+
+        falseButton.setOnClickListener {
+            if (!answered) {
+                checkAnswer(false, feedbackText)
+                answered = true
+            }
+        }
+
+        nextButton.setOnClickListener {
+            currentQuestions++
+            if (currentQuestions < questions.size) {
+                showQuestionScreen()
+            } else {
+                showQuestionScreen()
+            }
+        }
+    }
+
+    private fun checkAnswer(userAnswer: Boolean, feedbackText: TextView){
+        val correct = answers[currentQuestions]
+        if (userAnswer == correct) {
+            feedbackText.text = "Correct"
+            score++
+            feedbackList.add("Q${currentQuestions + 1}")
+        }else {
+            feedbackText.text = "Incorrect"
+            feedbackList.add("Q${currentQuestions + 1}")
+        }
+    }
+
+    private fun showScoreScreen(){
+        setContentView(R.layout.act_score)
+    }
 }
+
+
+
+
+
+
